1c0ce5797290e887ef0dba61f566c105dbcb3c5b
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-06-08 18:56:04 -0400

    completed Mastermind

--- mastermind.rb
+++ mastermind.rb
@@ -74,6 +74,24 @@ class Game
     end
 
     def get_code
+        valid_input = false
+        until valid_input
+            valid_input = true
+            valid_chars = "GBPROY"
+            puts
+            puts "(G)reen, (B)lue, (P)urple, (R)ed, (O)range, (Y)ellow"
+            puts "Enter your code (case insensitive) with no spaces:"
+            code = gets.chomp.upcase
+            if code.length != 4
+                valid_input = false
+            end
+            code.chars.each do |char|
+                if !valid_chars.include?(char)
+                    valid_input = false
+                end
+            end
+        end
+        code
     end
 
     def play_as_codebreaker
@@ -87,7 +105,7 @@ class Game
                 guess = ""
                 until guess == "G" || guess == "B" || guess == "P" || guess == "R" || guess == "O" || guess == "Y"
                     puts "(G)reen, (B)lue, (P)urple, (R)ed, (O)range, (Y)ellow"
-                    puts "What is your guess for position #{position}?"
+                    puts "What is your (case insensitive) guess for position #{position}?"
                     guess = gets.chomp.upcase
                 end
                 guess_code += guess
@@ -112,8 +130,80 @@ class Game
     end
 
     def play_as_codemaker
+        game_won = false
+        turn = 1
+        known_positions = ["", "", "", ""]
+        unknown_positions = []
+        colors = ["G", "B", "P", "R", "O", "Y"]
+        previous_guesses = Hash.new {|key, value| key[value]=[]}
+        until game_won || turn > 12
+            guess_code = ""
+            puts
+            puts "Turn #{turn}"
+            for position in 0..3 do
+                if known_positions[position] != ""
+                    guess_code += known_positions[position]
+                elsif unknown_positions.length > 0
+                    good_move = false
+                    attempts = 0
+                    until good_move || attempts == 10
+                        good_move = true
+                        guess_char = unknown_positions.sample
+                        if previous_guesses[position].include?(guess_char)
+                            good_move = false
+                            attempts += 1
+                        else
+                            unknown_positions.delete(guess_char)
+                        end
+                    end
+                    guess_code += guess_char
+                else
+                    good_move = false
+                    until good_move
+                        good_move = true
+                        guess_char = colors.sample
+                        if previous_guesses[position].include?(guess_char)
+                            good_move = false
+                        end
+                    end
+                    guess_code += guess_char
+                end
+            end
+            result = @code.check_code(guess_code)
+            puts
+            puts "+ means computer's guess in a position is correct, ? means position is incorrect"
+            puts "The computer's guess was: #{guess_code}"
+            puts "The results are:          #{result}"
+            if result == "++++"
+                game_won = true
+            else
+                turn += 1
+                result.chars.each_with_index do |char, position|
+                    if char == "+"
+                        known_positions[position] = guess_code[position]
+                        unknown_positions.delete(guess_code[position])
+                    end
+                end
+                result.chars.each_with_index do |char, position|
+                    if char == "?"
+                        unknown_positions.push(guess_code[position])
+                        previous_guesses[position].push(guess_code[position])
+                    elsif char == "-"
+                        previous_guesses[position].push(guess_code[position])
+                    end
+                end
+                print previous_guesses
+                puts
+                puts "Press Enter to continue."
+                gets
+            end
+        end
+        if game_won
+            puts "You lose! The computer used #{turn}/12 guesses."
+        else
+            puts "The computer ran out of guesses. You win!"
+        end
     end
-
 end
 
 game = Game.new
